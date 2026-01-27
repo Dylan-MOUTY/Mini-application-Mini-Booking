ed41188477c3fa4fbcd6c2ae2a156c97fa511565
RomainVoynier <romain.voynier@ynov.com>
2026-01-27 14:07:41 +0100

    start

---
+++ location/Villa.java
@@ -0,0 +1,26 @@
+public class Villa extends Hebergement {
+
+    private double prixSemaine;
+
+    public Villa(String id, int capacite, double prixSemaine) {
+        super(id, capacite);
+        this.prixSemaine = prixSemaine;
+    }
+
+    @Override
+    public double calculerPrix(Date debut, Date fin, int nbPersonnes) {
+        return prixSemaine + (nbPersonnes * 50);
+    }
+
+    @Override
+    public void afficherDetails() {
+        System.out.println("Villa " + identifiant +
+                " | Capacité : " + capacite +
+                " | Prix/semaine : " + prixSemaine);
+    }
+
+    @Override
+    public String getType() {
+        return "Villa";
+    }
+}
